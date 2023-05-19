f4879be2a4dc60d869e0ed1c7dd5f590cca78f8d
sshubhamk <kanadjeshubham@gmail.com>
2023-05-19 18:39:15 +0530

    TASK 2

--- libs/books/data-access/src/lib/+state/books.effects.spec.ts
+++ libs/books/data-access/src/lib/+state/books.effects.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 import { ReplaySubject } from 'rxjs';
@@ -28,18 +28,17 @@ describe('BooksEffects', () => {
   });
 
   describe('loadBooks$', () => {
-    it('should work', done => {
+    it('should return books matching search input', fakeAsync(() => {
       actions = new ReplaySubject();
       actions.next(BooksActions.searchBooks({ term: '' }));
-
       effects.searchBooks$.subscribe(action => {
         expect(action).toEqual(
           BooksActions.searchBooksSuccess({ books: [createBook('A')] })
         );
-        done();
       });
-
+      tick(500);
       httpMock.expectOne('/api/books/search?q=').flush([createBook('A')]);
-    });
+      flush();
+    }));
   });
 });
